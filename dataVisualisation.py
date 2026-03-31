6d1135d34a06342c6ad6292285ac65cd7b5668a5
Lajla <suljiclajla@gmail.com>
2026-03-31 11:56:24 +0200

    Implement sestavi_podatke function for data compilation
    Add data processing function to compile packet data.

--- dataVisualisation.py
+++ dataVisualisation.py
@@ -8,4 +8,27 @@ TIP_ZLOGA = {
 }
 
 def sestavi_podatke(seznam_paketov):
+    paket_seznam = []
+    st_vz_seznam = []
+    matrike = []
+
+    for i, paket in enumerate(seznam_paketov):
+        st_vzorcev = paket.data.shape[0]
+        st_vz_seznam.append(st_vzorcev)
+        matrike.append(paket.data)
+
+        if i<len(seznam_paketov)-1:
+            dt = seznam_paketov[i+1].ts - paket.ts
+            paket_seznam.append((paket.id, st_vzorcev, dt))
+
+    paket_avg = np.mean(st_vz_seznam)
+    st_vz_avg = np.mean(st_vz_seznam)
+    fvz = st_vz_avg /paket_avg
+
+    signal = np.vstack(matrike)
+
+    return fvz, signal
+
+if __name__ == "__main__":
     pass
+    
