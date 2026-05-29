5a23583c505aa9a7aebebcf0e8c1feb25a623893
Lara <larapodgorsek100@gmail.com>
2026-05-29 12:34:09 +0200

    Imporve tof model

--- model_tof/model.py
+++ model_tof/model.py
@@ -1,18 +1,19 @@
 import torch.nn as nn
 
+
 class ObstacleCNN(nn.Module):
-    def __init__(self, T=40, num_classes=5):
+    def __init__(self, num_classes=5):
         super().__init__()
         self.backbone = nn.Sequential(
-            nn.Conv1d(2, 32, kernel_size=5, padding=2),
+            nn.Conv1d(3, 32, kernel_size=5, padding=2),
             nn.BatchNorm1d(32),
             nn.ReLU(),
-            nn.MaxPool1d(2),          
+            nn.MaxPool1d(2),
 
             nn.Conv1d(32, 64, kernel_size=3, padding=1),
             nn.BatchNorm1d(64),
             nn.ReLU(),
-            nn.MaxPool1d(2),          
+            nn.MaxPool1d(2),
 
             nn.Conv1d(64, 128, kernel_size=3, padding=1),
             nn.BatchNorm1d(128),
@@ -24,11 +25,11 @@ class ObstacleCNN(nn.Module):
             nn.Linear(128, 64),
             nn.ReLU(),
             nn.Dropout(0.4),
-            nn.Linear(64, num_classes)
+            nn.Linear(64, num_classes),
         )
 
     def forward(self, x):
-        x = x.permute(0, 2, 1)  
+        x = x.permute(0, 2, 1)
         x = self.backbone(x)
         x = self.pool(x)
         return self.head(x)
