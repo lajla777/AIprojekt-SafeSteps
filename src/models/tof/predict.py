0b131f3ac0d8a5e802361ad8ca5d204af057f615
Lara <larapodgorsek100@gmail.com>
2026-05-29 22:16:33 +0200

    Refactor: migrate project to src layout

--- src/models/tof/predict.py
+++ src/models/tof/predict.py
@@ -1,6 +1,6 @@
 import torch
 import numpy as np
-from models.tof.model import ObstacleCNN
+from src.models.tof.model import ObstacleCNN
 
 LABEL_NAMES = ["no_obstacle", 
                "obstacle_left", 
