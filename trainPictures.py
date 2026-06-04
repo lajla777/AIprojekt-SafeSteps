357b6e6b49108da89ff591f60fe77e5a2d7e2b9f
lajla777 <suljiclajla@gmail.com>
2026-06-04 23:48:03 +0200

    nove slike za mrezo, izboljšana verzija, trenutno se train-4 se ustvarja zato ni commitan cel

--- trainPictures.py
+++ trainPictures.py
@@ -1,10 +1,10 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8n.pt")
+model = YOLO("yolov8s.pt")
 
 model.train(
     data="data.yaml",
-    epochs=50,
-    imgsz=640,
+    epochs=140,
+    imgsz=832,
     batch=8
 )
